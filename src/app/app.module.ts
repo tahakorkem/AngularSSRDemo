1a9ea1fa60ccb8696dd874830e5a6bded3498e9e
Taha Körkem <thkorkem@gmail.com>
2023-06-06 16:26:31 +0300

    Add required components for routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule, provideClientHydration} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {CartComponent} from './cart/cart.component';
+import {ProductComponent} from './product/product.component';
 import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CartComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
@@ -14,4 +20,5 @@ import {AppRoutingModule} from "./app-routing.module";
   providers: [provideClientHydration()],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
