2434d0fcb17d27af184c9b3dcbe41e6c0441705d
Taha Körkem <thkorkem@gmail.com>
2023-06-06 16:25:45 +0300

    Add routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./home/home.component";
+import {ProductComponent} from "./product/product.component";
+import {CartComponent} from "./cart/cart.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'product/:id',
+    component: ProductComponent,
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule, provideClientHydration} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent]
