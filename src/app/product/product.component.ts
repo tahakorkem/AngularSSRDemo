51787c3a74c7fbbbe8e89deb8d3dad5582d1ad70
Taha Körkem <thkorkem@gmail.com>
2023-06-09 16:29:52 +0300

    Fetch related products in product component

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, Inject, PLATFORM_ID} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {ProductService} from "../services/product/product.service";
 import {Product} from "../models/product";
 
 @Component({
@@ -9,5 +11,9 @@ import {Product} from "../models/product";
 export class ProductComponent {
 
   product = this.route.snapshot.data['product'] as Product
+  relatedProducts$ = this.productService.getRelatedProducts(this.product.id)
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object, private route: ActivatedRoute, private productService: ProductService) {
+  }
 
 }
