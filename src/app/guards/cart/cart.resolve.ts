ec370048d64c99aede5dee995fa3b49e0b2fbf36
Taha Körkem <thkorkem@gmail.com>
2023-06-09 16:28:34 +0300

    Use cart service in cart items resolver

--- src/app/guards/cart/cart.resolve.ts
+++ src/app/guards/cart/cart.resolve.ts
@@ -1,17 +1,9 @@
 import {ResolveFn} from "@angular/router";
-import {Observable} from "rxjs";
+import {inject} from "@angular/core";
+import {CartItem} from "../../models/cart-item";
+import {CartService} from "../../services/cart/cart.service";
 
-export const cartItemsResolver: ResolveFn<{ id: string, name: string, quantity: number }[]> = (route, state) => {
-  return new Observable(subscriber => {
-    setTimeout(() => {
-      subscriber.next([
-        {id: '1', name: 'Product 1', quantity: 1},
-        {id: '2', name: 'Product 2', quantity: 2},
-        {id: '3', name: 'Product 3', quantity: 3},
-        {id: '4', name: 'Product 4', quantity: 1},
-        {id: '5', name: 'Product 5', quantity: 2},
-      ])
-      subscriber.complete()
-    }, 1000)
-  })
+export const cartItemsResolver: ResolveFn<CartItem[]> = (route, state) => {
+  const cartService = inject(CartService);
+  return cartService.getCartItems()
 }
