c44aa83b58538efd6bd7b91691f93e5ae665b2a1
Taha Körkem <thkorkem@gmail.com>
2023-06-09 16:32:24 +0300

    Add routeReuseStrategy to product component

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,5 @@
 import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {ProductService} from "../services/product/product.service";
 import {isPlatformBrowser} from "@angular/common";
 import {Product} from "../models/product";
@@ -9,7 +9,7 @@ import {Product} from "../models/product";
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit {
 
   product = this.route.snapshot.data['product'] as Product
   platform = isPlatformBrowser(this.platformId) ? 'Browser' : 'Server'
@@ -17,7 +17,11 @@ export class ProductComponent {
     ? this.productService.getRelatedProducts(this.product.id)
     : undefined
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object, private route: ActivatedRoute, private productService: ProductService) {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object, private route: ActivatedRoute, private productService: ProductService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
   }
 
 }
