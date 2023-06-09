11aa1593c680faefbd5ace37ce4b3a84e3d33e1e
Taha Körkem <thkorkem@gmail.com>
2023-06-09 16:36:54 +0300

    Use config data in home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ConfigService} from "../services/config/config.service";
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  config = this.configService.config
+
+  constructor(private configService: ConfigService) {
+  }
 
 }
